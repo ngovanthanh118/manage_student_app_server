92532c1edaab29c24f539b3bd33ac00917cbd11d
ngovanthanh118 <nvthanh118@gmail.com>
2023-11-05 21:29:38 +0700

    deploy again

--- untils/connection.js
+++ untils/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 const connect = async () => {
     const uri = process.env.DB_URL || "mongodb+srv://nvthanh118:vanthanh118@cluster0.xvqzvwo.mongodb.net/manage_student_app";
-    await mongoose.set('bufferCommands', false);
     try {
-        await mongoose.connect(uri, { useNewUrlParser: true })
+        await mongoose.connect(uri, { bufferCommands: false })
             .then(() => console.log('Connected to database successfully'))
             .catch((err) => console.error(err))
     } catch (error) {
