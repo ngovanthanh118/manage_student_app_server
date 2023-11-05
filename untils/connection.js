c88d6ed3e4c34d8528731cdd3d989cd545b70613
ngovanthanh118 <nvthanh118@gmail.com>
2023-11-06 00:52:57 +0700

    deploy

--- untils/connection.js
+++ untils/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const Student = require('../models/student.js');
 const connect = async () => {
     const uri = process.env.DB_URL;
     try {
         await mongoose.connect(uri)
             .then(() => console.log('Connected to database successfully'))
-            .catch((err) => console.error(err))
+            .catch((err) => console.error(err));
+        await Student;
     } catch (error) {
         handleError(error);
     }
