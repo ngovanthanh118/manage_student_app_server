37619eda7770fd6face4a5486c5acfdf37cf1a98
ngovanthanh118 <nvthanh118@gmail.com>
2023-11-06 01:10:04 +0700

    deploy

--- untils/connection.js
+++ untils/connection.js
@@ -3,8 +3,8 @@ const Student = require('../models/student.js');
 const connect = async () => {
     const uri = process.env.DB_URL;
     try {
-        await mongoose.connect(uri)
-            .then(() => console.log('Connected to database successfully'))
+        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+            .then(() => console.log('Connected to database successfully!'))
             .catch((err) => console.error(err));
         await Student;
     } catch (error) {
