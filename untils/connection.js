232b2ee78b96467a7d706b981c101afd09713395
ngovanthanh118 <nvthanh118@gmail.com>
2023-11-06 00:26:11 +0700

    deploy app

--- untils/connection.js
+++ untils/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connect = async () => {
     const uri = process.env.DB_URL || "mongodb+srv://nvthanh118:vanthanh118@cluster0.xvqzvwo.mongodb.net/manage_student_app";
     try {
-        await mongoose.connect(uri, { bufferCommands: false })
+        await mongoose.connect(uri)
             .then(() => console.log('Connected to database successfully'))
             .catch((err) => console.error(err))
     } catch (error) {
